eb4c7e8549034dcaabd9d1305e9bc82744078c64
mherring <mherr170@gmail.com>
2020-05-06 18:18:35 -0400

    Add project files.

---
+++ GameOfWar/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameOfWar
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to close this program.");
+
+            //Pause the Console from closing
+            Console.ReadKey();
+
+        }
+    }
+}
